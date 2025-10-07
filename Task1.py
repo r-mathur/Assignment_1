a0011e6eb91d6ef151db2ff882b25cd4e699e12a
r-mathur <rishabhmathur106@outlook.com>
2025-10-07 12:11:20 +0530

    Assignment 1 first commit

---
+++ Task1.py
@@ -0,0 +1,12 @@
+first_number = int(input('Enter First Number: '))
+second_number = int(input('Enter Second Number: '))
+
+addition = first_number+second_number
+subtraction = first_number-second_number
+multiplication = first_number*second_number
+division = first_number/second_number
+
+print('Addition:',addition)
+print('Subtraction:',subtraction)
+print('Multiplication:',multiplication)
+print('Division:',division)
